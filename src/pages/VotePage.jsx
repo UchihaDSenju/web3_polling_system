ac226d687768ff6ce5b1140cdebfc9309ad33d8f
Tariq <rafiqtariq19@gmail.com>
2024-02-15 23:20:59 +0530

    vote status at etherscan link added

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -4,20 +4,27 @@ import { CandidateCard } from '../components'
 
 const VotePage = () => {
   const context = useContext(StateContext)
+  const checker = context.checkWallet()
+  const url = "https://sepolia.etherscan.io/address/" + context.currentAccount
   useEffect(() => {
     context.getCandidates();
   }, [])
   console.log(context.candidates);
   const candidates = context.candidates;
-  return (
-    <div>
-      {candidates.map((candidate, index) => {
-        return(
-          <CandidateCard key={index} imageUrl={candidate.logo} candidateName={candidate.name} qualification={candidate.education} partyName={candidate.party} />
-        )
-      })}
-    </div>
-  )
+  if(checker){
+    return (
+      <div>
+        {candidates.map((candidate, index) => {
+          return(
+            <CandidateCard key={index} imageUrl={candidate.logo} candidateName={candidate.name} qualification={candidate.education} partyName={candidate.party} />
+          )
+        })}
+        <div className='mx-auto w-fit bg-yellow-500 p-2 text-amber-800'>
+          <h1>After voting check status <a href={url} target='_blank'><span className='text-blue-600'>here</span></a></h1>
+        </div>
+      </div>
+    )
+  } 
 }
 
 export default VotePage
