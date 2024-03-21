2adf792541a6e5d4f33707e829abefb7eae57082
Tariq <rafiqtariq19@gmail.com>
2024-03-21 19:59:51 +0530

    Some design changes in Home and connectButton

--- src/components/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton.jsx
@@ -4,8 +4,8 @@ import { StateContext } from '../context'
 const ConnectWalletButton = () => {
     const context = useContext(StateContext)
   return (
-    <div className=''>
-        <button onClick={context.connectWallet} className='bg-red-600 rounded-xl text-center px-7 text-gray-300' >
+    <div className='h-[80vh] flex justify-center items-center '>
+        <button onClick={context.connectWallet} className='text-center bg-yellow-500 rounded-sm p-5 text-amber-900' >
             Connect Wallet Now
         </button>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
 import React, { useContext } from 'react'
 import { StateContext } from '../context'
+import { FaAndroid, FaWindows } from "react-icons/fa";
+import { IoLogoAndroid, IoLogoWindows } from "react-icons/io";
 
 const Home = () => {
     const context = useContext(StateContext);
     context.getOwnerAddress();
   return (
-    <div>The Context is {context.formData.name}</div>
+    <>
+      <div>
+        
+
+        <div class="border-b border-gray-200 dark:border-gray-700 w-[25%] mx-auto">
+            <ul class="flex justify-center -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                <li class="me-2">
+                    <a href="#" class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group">
+                        <IoLogoWindows className='text-2xl mr-2'/><h1>Windows</h1>
+                    </a>
+                </li>
+                <li class="me-2">
+                    <a href="#" class="inline-flex items-center justify-center p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" aria-current="page">
+                        <IoLogoAndroid className='text-3xl mr-2'/><h1>Android</h1>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+      </div>
+    </>
   )
 }
 
